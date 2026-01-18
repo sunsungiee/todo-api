1d7e3c762f806c38816e51df68fe37498a8193e0
sunsungiee <sunsungie@yandex.ru>
2026-01-18 16:03:04 +0600

    initial commit: todo api with laravel sail

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\JsonResponse;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        $tasks = Task::latest()->get();
+
+        return response()->json(TaskResource::collection($tasks));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(TaskRequest $request): JsonResponse
+    {
+        $task = Task::create($request->validated());
+        return response()->json($task, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task): JsonResponse
+    {
+        return response()->json(new TaskResource($task));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(TaskRequest $request, Task $task): JsonResponse
+    {
+        $task->update(
+            $request->validated()
+        );
+
+        return response()->json(
+            new TaskResource($task)
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task): JsonResponse
+    {
+        $task->delete();
+
+        return response()->json([
+            'message' => 'Task deleted successfully'
+        ]);
+    }
+}
---
+++ app/Models/Task.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use SoftDeletes;
+
+     protected $fillable = ['title', 'description', 'status'];
+}
