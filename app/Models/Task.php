ed2b31cf3424efa2b539d88316f8c123a1af0188
sunsungiee <sunsungie@yandex.ru>
2026-01-18 20:42:28 +0600

    add HasFactory to TaskModel

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    use HasFactory;
     use SoftDeletes;
 
      protected $fillable = ['title', 'description', 'status'];
